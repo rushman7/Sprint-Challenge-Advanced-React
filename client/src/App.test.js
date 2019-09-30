76f9fa83e449d8f6835696cbeeebc9a288822f6e
BuildTools <arthurpisakhov1@gmail.com>
2019-09-30 13:33:39 -0400

    added testing

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,9 +1,16 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import * as rtl from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect'
 import App from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
+describe('App', () => {
+  it('renders a span with the text Hello World', () => {
+    const wrapper = rtl.render(<App />);
+    const element = wrapper.queryByText(/hello world/i);
+
+    console.log(element.textContent);
+    expect(element).toBeInTheDocument();
+    expect(element).toBeTruthy();
+    expect(element).toBeVisible();
+  })
+})
--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,6 +1,17 @@
 import React from 'react';
 import { useDarkMode } from '../hooks/useDarkMode';
 import Switch from '@material-ui/core/Switch';
+import { ThemeProvider } from '@material-ui/styles';
+import { createMuiTheme } from '@material-ui/core/styles';
+import cyan from '@material-ui/core/colors/cyan';
+import green from '@material-ui/core/colors/green';
+
+const theme = createMuiTheme({
+  palette: {
+    primary: cyan,
+    secondary: green,
+  },
+});
 
 const NavBar = () => {
   const [darkMode, setDarkMode] = useDarkMode(false);
@@ -13,21 +24,23 @@ const NavBar = () => {
     setState({ ...state, [name]: event.target.checked });
   };
 
-  const toggleMode = e => {
+  const toggleMode = () => {
     setDarkMode(!darkMode);
   };
 
   return (
     <nav className="navbar">
       <h1>User Data</h1>
-        <Switch
-          checked={state.checkedA}
-          onClick={toggleMode}
-          onChange={handleChange('checkedA')}
-          value="checkedA"
-          color="primary"
-          inputProps={{ 'aria-label': 'primary checkbox' }}
-        />
+        <ThemeProvider theme={theme}>
+          <Switch
+            checked={state.checkedA}
+            onClick={toggleMode}
+            onChange={handleChange('checkedA')}
+            value="checkedA"
+            color="primary"
+            inputProps={{ 'aria-label': 'primary checkbox' }}
+          />
+        </ThemeProvider>
     </nav>
   );
 };
